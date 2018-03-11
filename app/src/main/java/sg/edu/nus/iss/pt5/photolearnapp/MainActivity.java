8fc349bfce2555b26769cd065a64c2aa1ab13edc
mjeyakaran <mjeyakaran@gmail.com>
2018-03-11 12:48:55 +0800

    Init Project

---
+++ app/src/main/java/sg/edu/nus/iss/pt5/photolearnapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.pt5.photolearnapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
